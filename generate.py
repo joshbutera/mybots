9d76a628c58cbb04e0fd3fc17f2a1f383cf7261d
joshbutera <joshuafbutera@gmail.com>
2023-01-22 18:19:15 -0600

    finished synapses

--- generate.py
+++ generate.py
@@ -3,6 +3,17 @@ import pyrosim.pyrosim as pyrosim
 length, width, height = 1, 1, 1
 x, y, z = 0, 0, height/2
 
+sensorNeurons = {
+   0: 'Torso',
+   1: 'BackLeg',
+   2: 'FrontLeg'
+}
+
+motorNeurons = {
+   3: 'Torso_BackLeg',
+   4: 'Torso_FrontLeg'
+}
+
 def Create_World():
    pyrosim.Start_SDF("world.sdf")
    pyrosim.Send_Cube(name="Box", pos=[-2,2,z] , size=[length,width,height])
@@ -28,6 +39,16 @@ def Generate_Brain():
    pyrosim.Send_Sensor_Neuron(name = 2 , linkName = "FrontLeg")
    pyrosim.Send_Motor_Neuron( name = 3 , jointName = "Torso_BackLeg")
    pyrosim.Send_Motor_Neuron( name = 4 , jointName = "Torso_FrontLeg")
+
+   # for neuron in sensorNeurons:
+   #    for motor in motorNeurons:
+   #       pyrosim.Send_Synapse( sourceNeuronName = neuron , targetNeuronName = motor , weight = 1.0 )
+
+
+   pyrosim.Send_Synapse( sourceNeuronName = 1 , targetNeuronName = 3 , weight = -1.0 )
+
+   pyrosim.Send_Synapse( sourceNeuronName = 0 , targetNeuronName = 4 , weight = -1.0 )
+   pyrosim.Send_Synapse( sourceNeuronName = 2 , targetNeuronName = 4 , weight = -1.0 )
 
    pyrosim.End()
 
