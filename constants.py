fe7dbc66e0e43c437af518511e24bc2da7fb2b2c
joshbutera <joshuafbutera@gmail.com>
2023-01-29 00:06:56 -0600

    finished quadruped

--- constants.py
+++ constants.py
@@ -5,3 +5,6 @@ frontLegAmplitude, frontLegFrequency, frontLegPhaseOffset = np.pi/4, 10, np.pi/4
 length = 1000
 numberOfGenerations = 10
 populationSize = 10
+numSensorNeurons = 9
+numMotorNeurons = 8
+motorJointRange = 0.4
