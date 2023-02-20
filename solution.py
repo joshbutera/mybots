b84b727d12fb2ce595bec2d704bed12eb0653ba2
joshbutera <joshuafbutera@gmail.com>
2023-02-20 12:10:50 -0600

    start of 7

--- solution.py
+++ solution.py
@@ -9,8 +9,9 @@ import constants as c
 class SOLUTION:
    def __init__(self):
       self.numLinks = np.random.randint(3,10)
-      self.dimensions = (np.random.rand(self.numLinks, 3).round(1) + 0.5) * 2
+      self.dimensions = (np.random.rand(self.numLinks, 3) + 0.5)
       self.hasSensor = np.random.randint(0,2,self.numLinks)
+      self.connections = [[] for i in range(self.numLinks)]
 
       self.maxHeight = self.dimensions[0][2]
       for dim in self.dimensions:
@@ -27,20 +28,21 @@ class SOLUTION:
       color = "blue"
       if self.hasSensor[0] == 1:
             color = "green"
-      pyrosim.Send_Cube(name='Cube0', pos=[0,0,self.maxHeight/2], size=self.dimensions[0], color=color)
-      pyrosim.Send_Joint(name='Cube0_Cube1', parent='Cube0', child='Cube1', type='revolute', position=[0,self.dimensions[0][1]/2,0], jointAxis="1 0 0")
+      pyrosim.Send_Cube(name='Cube0', pos=[0,0,self.maxHeight], size=self.dimensions[0], color=color)
 
-      for i in range(1, self.numLinks-1):
+      for i in range(1, self.numLinks):
          color = "blue"
          if self.hasSensor[i] == 1:
             color = "green"
-         pyrosim.Send_Cube(name='Cube'+str(i), pos=[0,self.dimensions[i][1]/2,self.maxHeight/2], size=self.dimensions[i], color=color)
-         pyrosim.Send_Joint(name='Cube'+str(i)+'_Cube'+str(i+1), parent='Cube'+str(i), child='Cube'+str(i+1), type='revolute', position=[0,self.dimensions[i][1],0], jointAxis="1 0 0")
-      
-      color = "blue"
-      if self.hasSensor[self.numLinks-1] == 1:
-         color = "green"
-      pyrosim.Send_Cube(name='Cube'+str(self.numLinks-1), pos=[0,self.dimensions[self.numLinks-1][1]/2,self.maxHeight/2], size=self.dimensions[self.numLinks-1], color=color)
+         connectedTo = np.random.randint(0, i)
+         self.connections[connectedTo].append(i)
+
+         temp = np.random.randint(0,3)
+         position = [0, 0, 0]
+         position[temp] = self.dimensions[connectedTo][temp] * random.choice([-1, 1])
+
+         pyrosim.Send_Joint(name='Cube'+str(connectedTo)+'_Cube'+str(i), parent='Cube'+str(connectedTo), child='Cube'+str(i), type='revolute', position=position, jointAxis="1 0 0")
+         pyrosim.Send_Cube(name='Cube'+str(i), pos=[0,0,self.maxHeight/2], size=self.dimensions[i], color=color)
 
       pyrosim.End()
 
@@ -53,8 +55,11 @@ class SOLUTION:
             pyrosim.Send_Sensor_Neuron(name = curr, linkName='Cube'+str(i))
             curr += 1
 
-      for i in range(self.numLinks-1):
-         pyrosim.Send_Motor_Neuron( name = i+curr , jointName = 'Cube'+str(i)+'_Cube'+str(i+1))
+      temp = curr
+      for i in range(len(self.connections)):
+         for j in self.connections[i]:
+            pyrosim.Send_Motor_Neuron( name = temp , jointName = 'Cube'+str(i)+'_Cube'+str(j))
+            temp += 1
 
       for i in range(self.numLinks):
          if self.hasSensor[i] == 1:
